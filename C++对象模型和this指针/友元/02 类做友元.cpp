937c0bb886a41c779b4d28f92f1ac0e60a870ea1
cy_wangjuntang@outlook.com <cy_wangjuntang@outlook.com>
2020-11-08 23:45:49 +0800

    学习

---
+++ C++对象模型和this指针/友元/02 类做友元.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//类做友元
+class Building;
+class GoodGay
+{
+public:
+	GoodGay();
+	void visit(); //参观函数，访问Building中的属性
+
+	Building* building;
+};
+
+class Building
+{
+	friend class GoodGay;
+public:
+	Building();
+
+public:
+	string m_SittingRoom; //客厅
+
+private:
+	string m_BedRoom; //卧室
+
+};
+
+//类外写成员函数
+Building::Building()
+{
+	m_SittingRoom = "客厅";
+	m_BedRoom = "卧室";
+}
+
+GoodGay::GoodGay()
+{
+	//创建建筑物对象
+	building = new Building;
+}
+
+void GoodGay::visit()
+{
+	cout << "好基友正访问：" << building->m_SittingRoom << endl;
+	cout << "好基友正访问：" << building->m_BedRoom << endl;
+}
+
+void test01()
+{
+	GoodGay gg;
+	gg.visit();
+}
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
