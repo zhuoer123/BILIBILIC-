46aa50ee65945d08ef9ada0909fcbb29ee6cea29
cy_wangjuntang@outlook.com <cy_wangjuntang@outlook.com>
2020-11-15 23:48:52 +0800

    学习

--- C++对象模型和this指针/运算符重载/02 左移运算符的重载.cpp
+++ C++对象模型和this指针/运算符重载/02 左移运算符的重载.cpp
@@ -1,39 +1,39 @@
-#include<iostream>
-using namespace std;
-
-//作用：输出自定义数据类型
-
-class Person
-{
-	friend ostream& operator <<(ostream& out, Person& p);
-public:
-	Person(int a, int b)
-	{
-		m_A = a;
-		m_B = b;
-	}
-private:
-	int m_A;
-	int m_B;
-};
-
-//只能利用全局函数重载左移运算符
-ostream & operator <<(ostream &out, Person &p) //函数类型加& 由于返回值是out 需要ostream &
-{
-	out << "m_A = " << p.m_A << "	m_B =" << p.m_B;
-	return out;
-}
-
-void test01()
-{
-	Person p(10, 10);
-	cout << p << "  Hello World!" << endl;
-
-}
-
-int main()
-{
-	test01();
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+////作用：输出自定义数据类型
+//
+//class Person
+//{
+//	friend ostream& operator <<(ostream& out, Person& p);
+//public:
+//	Person(int a, int b)
+//	{
+//		m_A = a;
+//		m_B = b;
+//	}
+//private:
+//	int m_A;
+//	int m_B;
+//};
+//
+////只能利用全局函数重载左移运算符
+//ostream & operator <<(ostream &out, Person &p) //函数类型加& 由于返回值是out 需要ostream &
+//{
+//	out << "m_A = " << p.m_A << "	m_B =" << p.m_B;
+//	return out;
+//}
+//
+//void test01()
+//{
+//	Person p(10, 10);
+//	cout << p << "  Hello World!" << endl;
+//
+//}
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}
