3f0de444ef11dd3bd0f2f7cab61460575f46e464
cy_wangjuntang@outlook.com <cy_wangjuntang@outlook.com>
2020-12-08 23:47:33 +0800

    学习

--- C++对象模型和this指针/运算符重载/04 赋值运算符重载.cpp
+++ C++对象模型和this指针/运算符重载/04 赋值运算符重载.cpp
@@ -1,61 +1,61 @@
-#include<iostream>
-using namespace std;
-
-class Person
-{
-public:
-	Person(int age)
-	{
-		m_Age = new int(age);
-	}
-
-	~Person()
-	{
-		if (m_Age != NULL)
-		{
-			delete m_Age;
-			m_Age = NULL;
-		}
-	}
-
-	Person& operator=(Person &p)
-	{
-		//编译器提供浅拷贝
-		//m_Age = p.m_Age;
-
-		//先判断是否有属性在堆区，如果有先释放干净，然后再深拷贝
-		if (p.m_Age != NULL)
-		{
-			delete m_Age;
-			m_Age = NULL;
-		}
-
-		//深拷贝
-		m_Age = new int(*p.m_Age);
-
-		return *this;
-	}
-
-	int* m_Age;
-};
-
-void test01()
-{
-	Person p1(18);
-	Person p2(20);
-	Person p3(20);
-
-	p3 = p2 = p1;
-
-	cout << "p1的年龄：" << *p1.m_Age << endl;
-	cout << "p2的年龄：" << *p2.m_Age << endl;
-	cout << "p3的年龄：" << *p3.m_Age << endl;
-}
-
-
-int main()
-{
-	test01();
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//class Person
+//{
+//public:
+//	Person(int age)
+//	{
+//		m_Age = new int(age);
+//	}
+//
+//	~Person()
+//	{
+//		if (m_Age != NULL)
+//		{
+//			delete m_Age;
+//			m_Age = NULL;
+//		}
+//	}
+//
+//	Person& operator=(Person &p)
+//	{
+//		//编译器提供浅拷贝
+//		//m_Age = p.m_Age;
+//
+//		//先判断是否有属性在堆区，如果有先释放干净，然后再深拷贝
+//		if (p.m_Age != NULL)
+//		{
+//			delete m_Age;
+//			m_Age = NULL;
+//		}
+//
+//		//深拷贝
+//		m_Age = new int(*p.m_Age);
+//
+//		return *this;
+//	}
+//
+//	int* m_Age;
+//};
+//
+//void test01()
+//{
+//	Person p1(18);
+//	Person p2(20);
+//	Person p3(20);
+//
+//	p3 = p2 = p1;
+//
+//	cout << "p1的年龄：" << *p1.m_Age << endl;
+//	cout << "p2的年龄：" << *p2.m_Age << endl;
+//	cout << "p3的年龄：" << *p3.m_Age << endl;
+//}
+//
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}
