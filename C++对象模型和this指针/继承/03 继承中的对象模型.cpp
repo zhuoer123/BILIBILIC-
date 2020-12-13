a1c0f26d675857038b13cfa7b42b4eda8b7dfef6
cy_wangjuntang@outlook.com <cy_wangjuntang@outlook.com>
2020-12-13 23:23:10 +0800

    学习

---
+++ C++对象模型和this指针/继承/03 继承中的对象模型.cpp
@@ -0,0 +1,43 @@
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+////利用开发人员命令提示工具查看对象模型
+////跳转盘符 D:
+////跳转文件路径 cd 具体文件路径下
+////查看命名
+//// cl /d1 reportSingleClassLayout类名 文件名
+//
+//
+//
+//class Base
+//{
+//public:
+//	int m_A;
+//protected:
+//	int m_B;
+//private:
+//	int m_C;
+//};
+//
+//class Son : public Base
+//{
+//public:
+//	int m_D;
+//};
+//
+//void test01()
+//{
+//	//Son s1;
+//	cout << "size of Son = " << sizeof(Son) << endl;  //16
+//}
+//
+//
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}
---
+++ C++对象模型和this指针/继承/04 构造与析构顺序.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Base
+{
+public:
+	Base()
+	{
+		cout << "Base的构造函数！" << endl;
+	}
+
+	~Base()
+	{
+		cout << "Base的析构函数！" << endl;
+	}
+};
+
+class Son : public Base
+{
+public:
+	Son()
+	{
+		cout << "Son的构造函数！" << endl;
+	}
+
+	~Son()
+	{
+		cout << "Son的析构函数！" << endl;
+	}
+};
+
+void test01()
+{
+	//Base b1;
+
+	//继承中的构造与析构顺序如下：
+	//先构造父类，再构造子类， 析构的顺序与构造的顺序相反
+	Son s1;
+
+}
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
