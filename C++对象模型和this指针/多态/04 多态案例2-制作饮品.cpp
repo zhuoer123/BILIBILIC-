2943db88367d5d748cfb2b8333c6162e60c5a91a
cy_wangjuntang@outlook.com <cy_wangjuntang@outlook.com>
2020-12-23 23:37:26 +0800

    学习

--- C++对象模型和this指针/多态/04 多态案例2-制作饮品.cpp
+++ C++对象模型和this指针/多态/04 多态案例2-制作饮品.cpp
@@ -1,111 +1,111 @@
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-//多态案例2 制作饮品
-class AbstractDrinking
-{
-public:
-
-	//煮水
-	virtual void Boil() = 0;
-
-	//冲泡
-	virtual void Brew() = 0;
-
-	//倒入杯中
-	virtual void PourInCup() = 0;
-
-	//加入辅料
-	virtual void PutSomething() = 0;
-
-	//制作饮品
-	void makeDrink()
-	{
-		Boil();
-		Brew();
-		PourInCup();
-		PutSomething();
-	}
-};
-
-class Coffee : public AbstractDrinking
-{
-public:
-	//煮水
-	virtual void Boil()
-	{
-		cout << "煮水" << endl;
-	}
-
-	//冲泡
-	virtual void Brew()
-	{
-		cout << "冲泡咖啡" << endl;
-	}
-
-	//倒入杯中
-	virtual void PourInCup()
-	{
-		cout << "倒入杯中" << endl;
-	}
-
-	//加入辅料
-	virtual void PutSomething()
-	{
-		cout << "加入糖和牛奶" << endl;
-	}
-};
-
-//制作茶叶
-class Tea : public AbstractDrinking
-{
-public:
-	//煮水
-	virtual void Boil()
-	{
-		cout << "煮水" << endl;
-	}
-
-	//冲泡
-	virtual void Brew()
-	{
-		cout << "冲泡茶叶" << endl;
-	}
-
-	//倒入杯中
-	virtual void PourInCup()
-	{
-		cout << "倒入杯中" << endl;
-	}
-
-	//加入辅料
-	virtual void PutSomething()
-	{
-		cout << "加入柠檬" << endl;
-	}
-};
-
-//制作函数
-void doWork(AbstractDrinking* drink)
-{
-	drink->makeDrink();
-}
-
-void test01()
-{
-	//制作咖啡
-	doWork(new Coffee);
-
-	cout << "-------------" << endl;
-	doWork(new Tea);
-}
-
-
-int main()
-{
-	test01();
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+////多态案例2 制作饮品
+//class AbstractDrinking
+//{
+//public:
+//
+//	//煮水
+//	virtual void Boil() = 0;
+//
+//	//冲泡
+//	virtual void Brew() = 0;
+//
+//	//倒入杯中
+//	virtual void PourInCup() = 0;
+//
+//	//加入辅料
+//	virtual void PutSomething() = 0;
+//
+//	//制作饮品
+//	void makeDrink()
+//	{
+//		Boil();
+//		Brew();
+//		PourInCup();
+//		PutSomething();
+//	}
+//};
+//
+//class Coffee : public AbstractDrinking
+//{
+//public:
+//	//煮水
+//	virtual void Boil()
+//	{
+//		cout << "煮水" << endl;
+//	}
+//
+//	//冲泡
+//	virtual void Brew()
+//	{
+//		cout << "冲泡咖啡" << endl;
+//	}
+//
+//	//倒入杯中
+//	virtual void PourInCup()
+//	{
+//		cout << "倒入杯中" << endl;
+//	}
+//
+//	//加入辅料
+//	virtual void PutSomething()
+//	{
+//		cout << "加入糖和牛奶" << endl;
+//	}
+//};
+//
+////制作茶叶
+//class Tea : public AbstractDrinking
+//{
+//public:
+//	//煮水
+//	virtual void Boil()
+//	{
+//		cout << "煮水" << endl;
+//	}
+//
+//	//冲泡
+//	virtual void Brew()
+//	{
+//		cout << "冲泡茶叶" << endl;
+//	}
+//
+//	//倒入杯中
+//	virtual void PourInCup()
+//	{
+//		cout << "倒入杯中" << endl;
+//	}
+//
+//	//加入辅料
+//	virtual void PutSomething()
+//	{
+//		cout << "加入柠檬" << endl;
+//	}
+//};
+//
+////制作函数
+//void doWork(AbstractDrinking* drink)
+//{
+//	drink->makeDrink();
+//}
+//
+//void test01()
+//{
+//	//制作咖啡
+//	doWork(new Coffee);
+//
+//	cout << "-------------" << endl;
+//	doWork(new Tea);
+//}
+//
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}
