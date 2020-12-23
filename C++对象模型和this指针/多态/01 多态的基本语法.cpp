4c050ec3a127f785be9df6e177fd3c9404b42d6b
1278380894@qq.com <1278380894@qq.com>
2020-12-23 15:13:43 +0800

    学习

--- C++对象模型和this指针/多态/01 多态的基本语法.cpp
+++ C++对象模型和this指针/多态/01 多态的基本语法.cpp
@@ -1,66 +1,72 @@
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-//多态
-
-//动物类
-class Animal
-{
-public:
-	//虚函数
-	virtual void speak()
-	{
-		cout << "动物在说话" << endl;
-	}
-};
-
-class Cat : public Animal
-{
-public:
-	//重写  函数返回值类型   函数名 参数列表 完全相同
-	void speak()
-	{
-		cout << "猫在说话" << endl;
-	}
-};
-
-class Dog : public Animal
-{
-public:
-	void speak()
-	{
-		cout << "狗在说话" << endl;
-	}
-};
-
-//执行说话的函数
-//地址早绑定 在编译阶段确定函数地址
-//如果想执行让猫说话，函数地址就不能提前绑定，需要在运行阶段进行绑定   地址晚绑定
-
-//动态多态满足条件：
-//1. 有继承关系
-//2. 子类重写父类的虚函数
-
-//动态多态的使用：
-// 父类的指针或引用  执行子类对象
-void doSpeak(Animal & animal)  // Animal & animal = cat   dog
-{
-	animal.speak();
-}
-
-void test01()
-{
-	Cat cat;
-	doSpeak(cat);
-	Dog dog;
-	doSpeak(dog);
-}
-
-int main()
-{
-	test01();
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+////多态
+//
+////动物类
+//class Animal
+//{
+//public:
+//	//虚函数
+//	virtual void speak()
+//	{
+//		cout << "动物在说话" << endl;
+//	}
+//};
+//
+//class Cat : public Animal
+//{
+//public:
+//	//重写  函数返回值类型   函数名 参数列表 完全相同
+//	void speak()
+//	{
+//		cout << "猫在说话" << endl;
+//	}
+//};
+//
+//class Dog : public Animal
+//{
+//public:
+//	void speak()
+//	{
+//		cout << "狗在说话" << endl;
+//	}
+//};
+//
+////执行说话的函数
+////地址早绑定 在编译阶段确定函数地址
+////如果想执行让猫说话，函数地址就不能提前绑定，需要在运行阶段进行绑定   地址晚绑定
+//
+////动态多态满足条件：
+////1. 有继承关系
+////2. 子类重写父类的虚函数
+//
+////动态多态的使用：
+//// 父类的指针或引用  执行子类对象
+//void doSpeak(Animal & animal)  // Animal & animal = cat   dog
+//{
+//	animal.speak();
+//}
+//
+//void test01()
+//{
+//	Cat cat;
+//	doSpeak(cat);
+//	Dog dog;
+//	doSpeak(dog);
+//}
+//
+//void test02()
+//{
+//	cout << "sizeof Animal = " << sizeof(Animal) << endl;
+//}
+//
+//int main()
+//{
+//	//test01();
+//	test02();
+//	system("pause");
+//	return 0;
+//}
---
+++ C++对象模型和this指针/多态/02 多态案例- 计算器实现.cpp
@@ -0,0 +1,124 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+//常规方法和多态法实现计算器
+//常规方法：
+class Calaulator
+{
+public:
+	int getResult(string oper)
+	{
+		if (oper == "+")
+		{
+			return m_Num1 + m_Num2;
+		}
+		else if(oper == "-")
+		{
+			return m_Num1 - m_Num2;
+		}
+		else if(oper == "*")
+		{
+			return m_Num1 * m_Num2;
+		}
+	}
+	//如果想要扩展功能，需修改源码
+	//真实开发中，提倡 开闭原则
+	//开闭原则：对扩展进行开发，对修改进行关闭
+
+	int m_Num1;
+	int m_Num2;
+};
+
+//利用多态实现计算器
+//1.组织结构清晰
+//2.可读性强
+//3.维护方便
+
+//实现计算器的抽象类
+class AbstractCalculator
+{
+public:
+	virtual int getResults()
+	{
+		return 0;
+	}
+	int m_Num1;
+	int m_Num2;
+};
+
+//加法计算器类
+class AddCalculator : public AbstractCalculator
+{
+public:
+	int getResults()
+	{
+		return m_Num1 + m_Num2;
+	}
+};
+
+//减法计算器类
+class SubCalculator : public AbstractCalculator
+{
+public:
+	int getResults()
+	{
+		return m_Num1 - m_Num2;
+	}
+};
+
+//乘法计算器类
+class MulCalculator : public AbstractCalculator
+{
+public:
+	int getResults()
+	{
+		return m_Num1 * m_Num2;
+	}
+};
+
+
+
+void test01()
+{
+	//创建计算器对象
+	Calaulator c;
+	c.m_Num1 = 10;
+	c.m_Num2 = 10;
+	cout << c.m_Num1 << " + " << c.m_Num2 << " = " << c.getResult("+") << endl;
+}
+
+void test02()
+{
+	//多态使用条件
+	//父类指针或者引用指向子类对象
+
+	AbstractCalculator* abc = new AddCalculator;
+	abc->m_Num1 = 100;
+	abc->m_Num2 = 100;
+	cout << abc->m_Num1 << " + " << abc->m_Num2 << " = " << abc->getResults() << endl;
+	delete abc;
+
+	//减法计算
+    abc = new SubCalculator;
+	abc->m_Num1 = 100;
+	abc->m_Num2 = 100;
+	cout << abc->m_Num1 << " - " << abc->m_Num2 << " = " << abc->getResults() << endl;
+	delete abc;
+
+	abc = new MulCalculator;
+	abc->m_Num1 = 100;
+	abc->m_Num2 = 100;
+	cout << abc->m_Num1 << " * " << abc->m_Num2 << " = " << abc->getResults() << endl;
+	delete abc;
+}
+
+
+int main()
+{
+	//test01();
+	test02();
+	system("pause");
+	return 0;
+}
