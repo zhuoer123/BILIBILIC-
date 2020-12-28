c1208d29c117b761b769947ab90e2e94422b0782
cy_wangjuntang@outlook.com <cy_wangjuntang@outlook.com>
2020-12-28 23:37:34 +0800

    学习

---
+++ C++对象模型和this指针/文件读写/01 文本文件-写文件.cpp
@@ -0,0 +1,34 @@
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//#include <fstream> 
+//
+////文本文件 写文件
+//void test01()
+//{
+//	//1. 包含头文件
+//
+//    //2. 创建流对象
+//	ofstream ofs;
+//
+//	//3.指定打开方式
+//	ofs.open("test.txt", ios::out);
+//
+//	//4.写内容
+//	ofs << "姓名：张三" << endl;
+//	ofs << "性别：男" << endl;
+//	ofs << "年龄：18" << endl;
+//
+//	//5. 关闭文件
+//	ofs.close();
+//}
+//
+//int main()
+//{
+//	test01();
+//	system("pause");
+//	return 0;
+//}
+
---
+++ C++对象模型和this指针/文件读写/02 文本文件-读文件.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+//文件操作 读文件
+void test01()
+{
+	//1. 包含头文件
+
+	//2. 创建流对象
+	ifstream ifs;
+
+	//3.打开文件，判断是否打开成功
+	ifs.open("test.txt", ios::in);
+
+	if (!ifs.is_open())
+	{
+		cout << "文件打开失败" << endl;
+		return;
+	}
+
+	//4. 读数据
+	//方法1：
+	//char buf[1024] = { 0 };
+	//while (ifs >> buf)
+	//{
+	//	cout << buf << endl;
+	//}
+	
+	//方法2：
+	//char buf[1024] = { 0 };
+	//while (ifs.getline(buf, sizeof(buf)))
+	//{
+	//	cout << buf << endl;
+	//}
+
+	//方法3：
+	string buf;
+	while (getline(ifs, buf))
+	{
+		cout << buf << endl;
+	}
+
+	//方法4： 不推荐
+	//char c;
+	//while ((c = ifs.get()) != EOF) //EOF -> end of file
+	//{
+	//	cout << c;
+	//}
+
+	//5. 关闭文件
+	ifs.close();
+}
+
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
